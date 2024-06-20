c4b815133bb206171e9919f72b7dc7e8e305f509
Cameron Davis <cam170@icloud.com>
2024-06-20 09:01:08 -0700

    added basic content

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/**/*.html", "./src/**/*.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
